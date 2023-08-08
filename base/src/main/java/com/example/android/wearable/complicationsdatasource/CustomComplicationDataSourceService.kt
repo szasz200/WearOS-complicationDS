a06412a65badc9df595bd9e57214b33fd6e637fa
szasz200 <73BFVebKLbL3JS7t>
2023-08-08 11:36:42 +0200

    first commit

---
+++ base/src/main/java/com/example/android/wearable/complicationsdatasource/CustomComplicationDataSourceService.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.wearable.complicationsdatasource
+
+import android.content.IntentFilter
+import android.util.Log
+import androidx.wear.watchface.complications.data.*
+import androidx.wear.watchface.complications.datasource.ComplicationRequest
+import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import java.util.*
+
+
+/**
+ * Example watch face complication data source provides a number that can be incremented on tap.
+ *
+ * Note: This class uses the suspending variation of complication data source service to support
+ * async calls to the data layer, that is, to the DataStore saving the persistent values.
+ */
+class CustomComplicationDataSourceService : SuspendingComplicationDataSourceService() {
+
+    /*
+         * Called when a complication has been activated. The method is for any one-time
+         * (per complication) set-up.
+         *
+         * You can continue sending data for the active complicationId until onComplicationDeactivated()
+         * is called.
+         */
+    override fun onComplicationActivated(
+        complicationInstanceId: Int,
+        type: ComplicationType
+    ) {
+        Log.d("hallo","hallo")
+        Log.d(TAG, "onComplicationActivated(): $complicationInstanceId")
+    }
+
+
+    /*
+     * A request for representative preview data for the complication, for use in the editor UI.
+     * Preview data is assumed to be static per type. E.g. for a complication that displays the
+     * date and time of an event, rather than returning the real time it should return a fixed date
+     * and time such as 10:10 Aug 1st.
+     *
+     * This will be called on a background thread.
+     */
+    override fun getPreviewData(type: ComplicationType): ComplicationData {
+        return ShortTextComplicationData.Builder(
+            text = PlainComplicationText.Builder(text = "6!").build(),
+            contentDescription = PlainComplicationText.Builder(text = "Short Text version of Number.").build()
+        )
+            .setTapAction(null)
+            .build()
+    }
+
+    /*
+     * Called when the complication needs updated data from your data source. There are four
+     * scenarios when this will happen:
+     *
+     *   1. An active watch face complication is changed to use this data source
+     *   2. A complication using this data source becomes active
+     *   3. The period of time you specified in the manifest has elapsed (UPDATE_PERIOD_SECONDS)
+     *   4. You triggered an update from your own class via the
+     *       ComplicationDataSourceUpdateRequester.requestUpdate method.
+     */
+    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
+        Log.d(TAG, "onComplicationRequest() id: ${request.complicationInstanceId}")
+
+        // Retrieves your data, in this case, we grab an incrementing number from Datastore.
+//        val number: Int = applicationContext.dataStore.data
+//            .map { preferences ->
+//                preferences[TAP_COUNTER_PREF_KEY] ?: 0
+//            }
+//            .first()
+        val number = 1
+        val numberText = String.format(Locale.getDefault(), "%d", number)
+        return when (request.complicationType) {
+
+            ComplicationType.SHORT_TEXT -> ShortTextComplicationData.Builder(
+                text = PlainComplicationText.Builder(text = numberText).build(),
+                contentDescription = PlainComplicationText
+                    .Builder(text = "Short Text version of Number.").build()
+            )//.setMonochromaticImage(MonochromaticImage.Builder())
+                .build()
+
+            else -> {
+                if (Log.isLoggable(TAG, Log.WARN)) {
+                    Log.w(TAG, "Unexpected complication type ${request.complicationType}")
+                }
+                null
+            }
+        }
+    }
+
+    /*
+     * Called when the complication has been deactivated.
+     */
+    override fun onComplicationDeactivated(complicationInstanceId: Int) {
+        Log.d(TAG, "onComplicationDeactivated(): $complicationInstanceId")
+    }
+
+    companion object {
+        private const val TAG = "CompDataSourceService"
+    }
+}
